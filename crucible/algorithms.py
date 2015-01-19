301cbeeed9ebfbd8f36198301ca88d0caa8ba402
Norbert Crombach <norbert.crombach@primetheory.org>
2015-01-19 20:31:29 +0100

    Import algorithms from Skyline HEAD and wrap lines

--- crucible/algorithms.py
+++ crucible/algorithms.py
@@ -11,12 +11,12 @@ from .settings import ALGORITHMS
 from os.path import dirname, join, abspath
 
 """
-This is no man's land. Do anything you want in here,
-as long as you return a boolean that determines whether the input
-timeseries is anomalous or not.
+This is no man's land. Do anything you want in here, as long as you return a
+boolean that determines whether the input timeseries is anomalous or not.
 
 To add an algorithm, define it here, and add its name to settings.ALGORITHMS.
 """
+
 
 def tail_avg(timeseries):
     """
@@ -25,11 +25,13 @@ def tail_avg(timeseries):
     It reduces noise, but it also reduces sensitivity and increases the delay
     to detection.
     """
+
     try:
         t = (timeseries[-1][1] + timeseries[-2][1] + timeseries[-3][1]) / 3
         return t
     except IndexError:
         return timeseries[-1][1]
+
 
 def median_absolute_deviation(timeseries):
     """
@@ -42,17 +44,18 @@ def median_absolute_deviation(timeseries):
     demedianed = np.abs(series - median)
     median_deviation = demedianed.median()
 
-    # The test statistic is infinite when the median is zero,
-    # so it becomes super sensitive. We play it safe and skip when this happens.
+    # The test statistic is infinite when the median is zero, so it becomes
+    # super sensitive. We play it safe and skip when this happens.
     if median_deviation == 0:
         return False
 
     test_statistic = demedianed.iget(-1) / median_deviation
 
-    # Completely arbitary...triggers if the median deviation is
-    # 6 times bigger than the median
+    # Completely arbitary...triggers if the median deviation is 6 times bigger
+    # than the median
     if test_statistic > 6:
         return True
+
 
 def grubbs(timeseries):
     """
@@ -65,18 +68,20 @@ def grubbs(timeseries):
     tail_average = tail_avg(timeseries)
     z_score = (tail_average - mean) / stdDev
     len_series = len(series)
-    threshold = scipy.stats.t.isf(.05 / (2 * len_series) , len_series - 2)
+    threshold = scipy.stats.t.isf(.05 / (2 * len_series), len_series - 2)
     threshold_squared = threshold * threshold
     grubbs_score = ((len_series - 1) / np.sqrt(len_series)) * np.sqrt(threshold_squared / (len_series - 2 + threshold_squared))
 
     return z_score > grubbs_score
 
+
 def first_hour_average(timeseries):
     """
-    Calcuate the simple average over one hour, FULL_DURATION seconds ago.
-    A timeseries is anomalous if the average of the last three datapoints
-    are outside of three standard deviations of this value.
+    Calcuate the simple average over one hour, FULL_DURATION seconds ago. A
+    timeseries is anomalous if the average of the last three datapoints are
+    outside of three standard deviations of this value.
     """
+
     last_hour_threshold = time() - (86400 - 3600)
     series = pandas.Series([x[1] for x in timeseries if x[0] < last_hour_threshold])
     mean = (series).mean()
@@ -85,13 +90,16 @@ def first_hour_average(timeseries):
 
     return abs(t - mean) > 3 * stdDev
 
+
 def stddev_from_average(timeseries):
     """
-    A timeseries is anomalous if the absolute value of the average of the latest
-    three datapoint minus the moving average is greater than one standard
-    deviation of the average. This does not exponentially weight the MA and so
-    is better for detecting anomalies with respect to the entire series.
+    A timeseries is anomalous if the absolute value of the average of the
+    latest three datapoint minus the moving average is greater than three
+    standard deviations of the average. This does not exponentially weight the
+    MA and so is better for detecting anomalies with respect to the entire
+    series.
     """
+
     series = pandas.Series([x[1] for x in timeseries])
     mean = series.mean()
     stdDev = series.std()
@@ -99,24 +107,27 @@ def stddev_from_average(timeseries):
 
     return abs(t - mean) > 3 * stdDev
 
+
 def stddev_from_moving_average(timeseries):
     """
-    A timeseries is anomalous if the absolute value of the average of the latest
-    three datapoint minus the moving average is greater than one standard
-    deviation of the moving average. This is better for finding anomalies with
-    respect to the short term trends.
+    A timeseries is anomalous if the absolute value of the average of the
+    latest three datapoint minus the moving average is greater than three
+    standard deviations of the moving average. This is better for finding
+    anomalies with respect to the short term trends.
     """
+
     series = pandas.Series([x[1] for x in timeseries])
     expAverage = pandas.stats.moments.ewma(series, com=50)
     stdDev = pandas.stats.moments.ewmstd(series, com=50)
 
     return abs(series.iget(-1) - expAverage.iget(-1)) > 3 * stdDev.iget(-1)
 
+
 def mean_subtraction_cumulation(timeseries):
     """
-    A timeseries is anomalous if the value of the next datapoint in the
-    series is farther than a standard deviation out in cumulative terms
-    after subtracting the mean from each data point.
+    A timeseries is anomalous if the value of the next datapoint in the series
+    is farther than three standard deviations out in cumulative terms after
+    subtracting the mean from each data point.
     """
 
     series = pandas.Series([x[1] if x[1] else 0 for x in timeseries])
@@ -126,10 +137,11 @@ def mean_subtraction_cumulation(timeseries):
 
     return abs(series.iget(-1)) > 3 * stdDev
 
+
 def least_squares(timeseries):
     """
-    A timeseries is anomalous if the average of the last three datapoints
-    on a projected least squares model is greater than three sigma.
+    A timeseries is anomalous if the average of the last three datapoints on a
+    projected least squares model is greater than three sigma.
     """
 
     x = np.array([t[0] for t in timeseries])
@@ -140,23 +152,24 @@ def least_squares(timeseries):
     m, c = np.linalg.lstsq(A, y)[0]
     errors = []
     for i, value in enumerate(y):
-    	projected = m * x[i] + c
-    	error = value - projected
-    	errors.append(error)
+        projected = m * x[i] + c
+        error = value - projected
+        errors.append(error)
 
     if len(errors) < 3:
-    	return False
+        return False
 
     std_dev = scipy.std(errors)
     t = (errors[-1] + errors[-2] + errors[-3]) / 3
 
     return abs(t) > std_dev * 3 and round(std_dev) != 0 and round(t) != 0
 
+
 def histogram_bins(timeseries):
     """
     A timeseries is anomalous if the average of the last three datapoints falls
-    into a histogram bin with less than 20 other datapoints (you'll need to tweak
-    that number depending on your data)
+    into a histogram bin with less than 20 other datapoints (you'll need to
+    tweak that number depending on your data)
 
     Returns: the size of the bin which contains the tail_avg. Smaller bin size
     means more anomalous.
@@ -178,11 +191,12 @@ def histogram_bins(timeseries):
 
     return False
 
+
 def ks_test(timeseries):
     """
     A timeseries is anomalous if 2 sample Kolmogorov-Smirnov test indicates
-    that data distribution for last 10 minutes is different from last hour.
-    It produces false positives on non-stationary series so Augmented
+    that data distribution for last 10 minutes is different from last hour. It
+    produces false positives on non-stationary series so Augmented
     Dickey-Fuller test applied to check for stationarity.
     """
 
@@ -194,11 +208,11 @@ def ks_test(timeseries):
     if reference.size < 20 or probe.size < 20:
         return False
 
-    ks_d,ks_p_value = scipy.stats.ks_2samp(reference, probe)
+    ks_d, ks_p_value = scipy.stats.ks_2samp(reference, probe)
 
     if ks_p_value < 0.05 and ks_d > 0.5:
         adf = sm.tsa.stattools.adfuller(reference, 10)
-        if  adf[1] < 0.05:
+        if adf[1] < 0.05:
             return True
 
     return False
