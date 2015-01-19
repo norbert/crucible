8f0c50502b83b12910755d3f59558ac10a8be166
Norbert Crombach <norbert.crombach@primetheory.org>
2015-01-19 19:37:57 +0100

    Import MIT license and setuptools configuration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='crucible',
+      version='0.0.1',
+      description='Forge your models out of steel.',
+      author='Abe Stanway',
+      author_email='me@abe.is',
+      maintainer='Norbert Crombach',
+      maintainer_email='norbert.crombach@primetheory.org',
+      license='MIT',
+      packages=['crucible'])
